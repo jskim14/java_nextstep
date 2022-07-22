2c4a3c8d67630eef07ad77b9036734301e4df930
KJS\user <jskim14@sanhait.co.kr>
2022-07-22 11:10:57 +0900

    todo 정리

--- src/main/java/bong/lines/basic/handler/common/DispatcherServlet.java
+++ src/main/java/bong/lines/basic/handler/common/DispatcherServlet.java
@@ -1,7 +1,12 @@
 package bong.lines.basic.handler.common;
 
+import bong.lines.basic.handler.common.mapping.DeleteMapping;
+import bong.lines.basic.handler.common.mapping.GetMapping;
 import bong.lines.basic.handler.common.mapping.PostMapping;
+import bong.lines.basic.handler.common.mapping.PutMapping;
 import bong.lines.basic.handler.common.mapping.mapper.HandlerMapping;
+import bong.lines.basic.handler.common.mapping.mapper.TypeHandling;
+import bong.lines.basic.handler.common.method.HTTP_METHOD;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.InputStream;
@@ -21,27 +26,29 @@ public class DispatcherServlet implements Runnable {
     public void run() {
         try(InputStream in = connection.getInputStream();
             OutputStream out = connection.getOutputStream()) {
+            
+            //todo inputstream은 이미 사용되었으니 또 불러올수가 없음 -> 타입핸들링에서 반환해서 보내줌
 
-//            String requestType = new TypeHandling(in, out).process().split(" ")[0];
-//            HTTP_METHOD httpMethod = HTTP_METHOD.of(requestType);
+            String requestType = new TypeHandling(in, out).process().split(" ")[0];
+            HTTP_METHOD httpMethod = HTTP_METHOD.of(requestType);
 
             HandlerMapping handlerMapping = null;
-//            switch (httpMethod) {
-//                case GET:
-//                    handlerMapping = new GetMapping(in, out);
-//                    break;
-//                case POST:
-//                    handlerMapping = new PostMapping(in, out);
-//                    break;
-//                case PUT:
-//                    handlerMapping = new PutMapping(in, out);
-//                    break;
-//                case DELETE:
-//                    handlerMapping = new DeleteMapping(in, out);
-//                    break;
-//            }
+            switch (httpMethod) {
+                case GET:
+                    handlerMapping = new GetMapping(in, out);
+                    break;
+                case POST:
+                    handlerMapping = new PostMapping(in, out);
+                    break;
+                case PUT:
+                    handlerMapping = new PutMapping(in, out);
+                    break;
+                case DELETE:
+                    handlerMapping = new DeleteMapping(in, out);
+                    break;
+            }
 
-            handlerMapping = new PostMapping(in, out);
+//            handlerMapping = new PostMapping(in, out);
             handlerMapping.process();
 
         }catch (Exception exception){
